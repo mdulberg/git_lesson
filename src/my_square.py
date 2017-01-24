d3eb97df2637b43b0f2be8976021ccb51a001b77
micky dulberg <mdulberg@gmail.com>
2017-01-24 06:35:21 -0800

    fix merge result

--- src/my_square.py
+++ src/my_square.py
@@ -1,11 +1,6 @@
 def my_square(x):
-<<<<<<< HEAD
-	""" takes number and returns square
+	""" takes number and returns square more doc
 	"""
-=======
-	"" takes number and returns square. added more documentation
-	""
->>>>>>> 80c09720dd0aac7e6a98416b941e9e1b0a57ec04
 	return(x ** 2)
 
 print(my_square(4))
