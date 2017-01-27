a66efd49f36c728015e474d9930f8e65c438a6b6
micky dulberg <mdulberg@gmail.com>
2017-01-27 05:50:10 -0800

    adding more scripts again

---
+++ python/class_example.py
@@ -0,0 +1,29 @@
+
+class calc:
+	def add(x,y):
+		answer = x+y
+		print(answer)
+	def sub(x,y):
+		answer = x-y
+		print(answer)
+	def mult(x,y):
+		answer = x*y
+		print(answer)
+	def div(x,y):
+		answer = x/y
+		print(answer)
+
+x = calc.add(3,4)
+print(x)
+
+y = calc.div(3,4)
+print(y)
+
+print(calc.mult(3,4))
+
+
+
+
+
+
+
